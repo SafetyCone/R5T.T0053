4ae2afa4fbaf9fad4e013dd9c8982b231a8112d3
davidcoats <david@getsafetycone.com>
2021-09-28 16:24:53 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0053.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0053.Construction
+{
+    /// <summary>
+    /// Construction project for IdentifierTypeCode and TypeCodedIdentifierOperator extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0053/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0053
+{
+    /// <summary>
+    /// IdentifierTypeCode and TypeCodedIdentifierOperator extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
